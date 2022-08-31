00179834b181ac72d068095a30170930e78dd36d
Vivek Tiwary <vivek.t@lsq.net>
2022-08-31 15:49:28 +0530

    first commit

---
+++ MonthlySalary/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var lib = new SomeOtherLib();
+
+Console.WriteLine(lib.Age);
+Console.WriteLine(lib.Time);
+
+
+
+
+
+
+
+class SomeOtherLib 
+{
+    public int Age { get; set; }
+    public int Time { get; set; }
+}
